5d626ef87fb692eace5c7101d33f21b783760cce
Dylan Chan <dchan2614@gmail.com>
2022-02-16 17:13:00 -0500

    removed content card stuff added stuff to content list

--- Dchan_Animals/src/app/content-card/content-card.component.ts
+++ Dchan_Animals/src/app/content-card/content-card.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { AnimalList } from '../helper-files/content-list';
 
 @Component({
   selector: 'app-content-card',
@@ -7,51 +6,10 @@ import { AnimalList } from '../helper-files/content-list';
   styleUrls: ['./content-card.component.scss']
 })
 export class ContentCardComponent implements OnInit {
-
-  animoo:AnimalList   //define variable called animoo and make it of the class type
+  
   
   constructor() {
     
-    this.animoo = new AnimalList;   //animoo is a new animallist
-    
-    this.animoo.add({ //adds new animal to the array
-      id:1,
-      title:"Koala",
-      description:"Dumb tree bear",
-      creator:"god",
-      imgURL:"https://c.files.bbci.co.uk/100E0/production/_114306756_gettyimages-482829557.jpg",
-      type:"bear",
-      tags:["koala","eucalyptus","small brain"]
-    });  
-
-    this.animoo.add({ //adds new animal to the array
-      id:2,
-      title:"dog",
-      description:"Doggo",
-      creator:"god",
-      imgURL:"https://i1.sndcdn.com/artworks-000810672895-5vv94q-t500x500.jpg",
-      type:"dog",
-      tags:["dogs","Doge","good boys"]
-    });
-
-    this.animoo.add({ //adds new animal to the array
-      id:3,
-      title:"lemur",
-      description:"likes garbonzo beans",
-      creator:"god",
-      imgURL:"https://scdn.nflximg.net/ipl/10973/8ca4580aaa23952b2723aa7e726ed4d34788888c.jpg",
-      type:"lemur",
-      tags:["lemur","Zoboomafoo","beans"]
-    });
-    this.animoo.add({ //adds new animal to the array
-      id:4,
-      title:"lemur",
-      description:"likes garbonzo beans",
-      creator:"god",
-      imgURL:"https://i.imgur.com/lETaPrS.jpg?1",
-      type:"lemur",
-      tags:["lemur","Zoboomafoo","beans"]
-    });
   }
 
   ngOnInit(): void {
---
+++ Dchan_Animals/src/app/content-list/content-list.component.ts
@@ -0,0 +1,73 @@
+import { Component, OnInit } from '@angular/core';
+import { IAnimals } from '../helper-files/content-interface';
+
+@Component({
+  selector: 'app-content-list',
+  templateUrl: './content-list.component.html',
+  styleUrls: ['./content-list.component.scss']
+})
+export class ContentListComponent implements OnInit {
+
+  animells:IAnimals[]
+
+  constructor() {
+    this.animells = [{
+      id:1,
+      title:"ayeaye",
+      description:"I cant hear youuuuuu",
+      creator:"jesus",
+      imgURL:"https://cdn.discordapp.com/attachments/369194512234446848/943626877396983848/xw0fkzv1r34yduw1lhkm.png",
+      type:"primate",
+      tags:["ayeaye captain","wtf is this shit","cursed lemur"]
+    },
+    {
+      id:2,
+      title:"kiwi",
+      description:"tasty fruit...chicken",
+      creator:"Sir Isaac Newton",
+      imgURL:"https://cdn.discordapp.com/attachments/369194512234446848/943627489866027008/tmp-name-2-31487-1502920343-3_dblbig.jpg",
+      type:"fruit",
+      tags:["round","green","discovered gravity"]
+    },
+    {
+      id:3,
+      title:"Goblin Shark",
+      description:"*evil screech",
+      creator:"Louis XIV of France",
+      imgURL:"https://cdn.discordapp.com/attachments/369194512234446848/943628251320946708/mitsukurina-owstoni-goblin-shark-registration-no-a-24744-180056-large_2_copy.jpg",
+      type:"fish?",
+      tags:["shark","Goblin slayer"]
+    },
+    {
+      id:4,
+      title:"???????????",
+      description:"???????????",
+      creator:"George W. Bush",
+      imgURL:"https://cdn.discordapp.com/attachments/369194512234446848/943629570228232262/latest.png",
+      type:"??????????",
+      tags:["??????","poop"]
+    },
+    {
+      id:5,
+      title:"Leonard",
+      description:"One Hungry Boi",
+      creator:"Thomas Jefferson",
+      imgURL:"https://cdn.discordapp.com/attachments/369194512234446848/943629797580476486/latest.png",
+      type:"Hungry Boi",
+      tags:["Hungry","#onadiet"]
+    },
+    {
+      id:6,
+      title:"Blue Devil",
+      description:"Gotta go fast",
+      creator:"Wolfgang Amadeus mozart",
+      imgURL:"https://2.bp.blogspot.com/-90OMNRv53r4/XBIfQuaAz_I/AAAAAAAAiAE/D8YOi38r1PUkCXDu5h1yAM4WNI8Z0aRwQCLcBGAs/s1600/live%2Bin%2Bdarkness.jpg",
+      type:"Speedster",
+      tags:["Sanic speed","Sanic","I am speed"]
+    }];
+  }
+
+  ngOnInit(): void {
+  }
+
+}
