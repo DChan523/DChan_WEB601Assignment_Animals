679942a30a129f0e45c0e22b38f0332268e8c62a
Dylan Chan <dchan2614@gmail.com>
2022-02-20 16:39:49 -0500

    tried learning @Input

--- Dchan_Animals/src/app/hover-affect.directive.ts
+++ Dchan_Animals/src/app/hover-affect.directive.ts
@@ -4,15 +4,22 @@ import { Directive, ElementRef, HostListener, Input } from '@angular/core';
   selector: '[appHoverAffect]'
 })
 export class HoverAffectDirective {
-  @Input() colour?:string;    //i still have no clue what @Input does
+  @Input() eggs?:string;    //i still have no clue what @Input does
 
   constructor(private elm:ElementRef){
       elm.nativeElement.style.cursor = "wait";
   }
 //ask alex for help this stuff only half makes sense its kinda like event listeners but ugly
   ngOnInit():void{}
-  @HostListener('mouseover')potato(btn:any){
-    this.highlightFunction(this.colour);
+  @HostListener('mouseenter')potato(btn:any){
+    this.highlightFunction(this.eggs);
+  }
+  @HostListener('mouseleave')tomato(btn:any){
+    this.remove(this.eggs);
+  }
+  private remove(anotherParameter?:string):void{
+    this.elm.nativeElement.style.fontWeight = 'normal';
+    this.elm.nativeElement.style.textDecoration = 'none';
   }
   private highlightFunction(someParameter?: string): void {
     this.elm.nativeElement.style.fontWeight = 'bold';
