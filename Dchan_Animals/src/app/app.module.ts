ca11988855b40acc9e488336763839bf1ebdf375
Dylan <dchan2614@gmail.com>
2022-03-06 20:04:41 -0500

    added form thing to the project and created new component

--- Dchan_Animals/src/app/app.module.ts
+++ Dchan_Animals/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { ContentCardComponent } from './content-card/content-card.component';
 import { ContentListComponent } from './content-list/content-list.component';
 import { HoverAffectDirective } from './hover-affect.directive';
+import { CreateContentComponent } from './create-content/create-content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContentCardComponent,
     ContentListComponent,
-    HoverAffectDirective
+    HoverAffectDirective,
+    CreateContentComponent
   ],
   imports: [
     BrowserModule
---
+++ Dchan_Animals/src/app/create-content/create-content.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, Output } from '@angular/core';
+import { EventEmitter } from '@angular/core';
+import { IAnimals } from '../helper-files/content-interface';
+
+@Component({
+  selector: 'app-create-content',
+  templateUrl: './create-content.component.html',
+  styleUrls: ['./create-content.component.scss']
+})
+export class CreateContentComponent implements OnInit {
+  //output sends data from child to parent eventemmiter is the thing that sends the data
+  @Output() newAnimalEvent:EventEmitter<IAnimals> = new EventEmitter<IAnimals>();
+
+  newAnimal?:IAnimals; //new animals!!! of whateever my interface is now
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  //something about adding content
+  addContent(id:string,title:string,description:string,creator:string,imgURL:string,type:string){
+    this.newAnimal = {
+      id:parseInt(id),
+      title:title,
+      description:description,
+      creator:creator,
+      imgURL:imgURL,
+      type:type,
+    };
+    //emit emits a value...
+    this.newAnimalEvent.emit(this.newAnimal);
+  }
+}
