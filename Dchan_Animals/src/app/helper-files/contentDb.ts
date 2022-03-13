003204264490ce1f401092f19ba4bf059638abc8
Dylan <dchan2614@gmail.com>
2022-03-13 17:49:13 -0400

    added stuff

---
+++ Dchan_Animals/src/app/helper-files/contentDb.ts
@@ -0,0 +1,65 @@
+import { IAnimals } from "./content-interface";
+
+
+    export const animells:IAnimals[] = [{
+      id:1,
+      title:"ayeaye",
+      description:"I cant hear youuuuuu",
+      creator:"jesus",
+      imgURL:"https://cdn.discordapp.com/attachments/369194512234446848/943626877396983848/xw0fkzv1r34yduw1lhkm.png",
+      type:"primate",
+      tags:["ayeaye captain","wtf is this shit","cursed lemur"]
+    },
+    {
+      id:2,
+      title:"kiwi",
+      description:"tasty fruit...chicken",
+      creator:"Sir Isaac Newton",
+      imgURL:"https://cdn.discordapp.com/attachments/369194512234446848/943627489866027008/tmp-name-2-31487-1502920343-3_dblbig.jpg",
+      type:"fruit",
+      tags:["round","green","discovered gravity"]
+    },
+    {
+      id:3,
+      title:"Goblin Shark",
+      description:"*evil screech",
+      creator:"Louis XIV of France",
+      imgURL:"https://cdn.discordapp.com/attachments/369194512234446848/943628251320946708/mitsukurina-owstoni-goblin-shark-registration-no-a-24744-180056-large_2_copy.jpg",
+      type:"fish?",
+      tags:["shark","Goblin slayer"]
+    },
+    {
+      id:4,
+      title:"???????????",
+      description:"???????????",
+      creator:"George W. Bush",
+      imgURL:"https://cdn.discordapp.com/attachments/369194512234446848/943629570228232262/latest.png",
+      type:"??????????",
+      tags:["??????","poop"]
+    },
+    {
+      id:5,
+      title:"Leonard",
+      description:"One Hungry Boi",
+      creator:"Thomas Jefferson",
+      imgURL:"https://cdn.discordapp.com/attachments/369194512234446848/943629797580476486/latest.png",
+      type:"Hungry Boi",
+      tags:["Hungry","#onadiet"]
+    },
+    {
+      id:6,
+      title:"Blue Devil",
+      description:"Gotta go fast",
+      creator:"Wolfgang Amadeus mozart",
+      type:"Speedster",
+      tags:["Sanic speed","Sanic","I am speed"]
+    },
+    {
+      id:7,
+      title:"Copy Pasta",
+      description:"pAsTA",
+      creator:"The tomato from Veggie Tales",
+      imgURL:"https://i.kym-cdn.com/entries/icons/original/000/000/246/copy.jpg",
+      tags:["spaghetti","Sauce","Veggie Tales"]
+    }];
+  }
