1c3da40e6e397955108c820675d98e5cbb63905b
Dylan <dchan2614@gmail.com>
2022-04-03 00:01:12 -0400

    moved stuff around

--- Dchan_Animals/src/app/dialog/dialog.component.ts
+++ Dchan_Animals/src/app/dialog/dialog.component.ts
@@ -8,40 +8,33 @@ import { EventEmitter } from '@angular/core';
   styleUrls: ['./dialog.component.scss']
 })
 export class DialogComponent implements OnInit {
-  @Output() newAnimalEvent: EventEmitter<IAnimals> = new EventEmitter<IAnimals>();
-  @Output() updateAnimalEvent: EventEmitter<IAnimals> = new EventEmitter<IAnimals>();
+  // @Output() newAnimalEvent: EventEmitter<IAnimals> = new EventEmitter<IAnimals>();
+  // @Output() updateAnimalEvent: EventEmitter<IAnimals> = new EventEmitter<IAnimals>();
  
-  newAnimal?:IAnimals; //new animals!!! of whateever my interface is now
-  constructor() { }
+  // newAnimal?:IAnimals; //new animals!!! of whateever my interface is now
+  // constructor() { }
 
   ngOnInit(): void {
   }
-  //ADD THE MODIFY COINTENT FUNCTIONS IN HERE
-  
-  //something about adding content
-  addContent(title:string,description:string,creator:string,imgURL:string,type:string){
-    this.newAnimal = {
-      title:title,
-      description:description,
-      creator:creator,
-      imgURL:imgURL,
-      type:type,
-    };
-    //emit emits a value...
-    //after some deeep thoughts i believe this is what acurally sends the data to the parent element
-    this.newAnimalEvent.emit(this.newAnimal);
-  }
-  updateContent(id:string,title:string,description:string,creator:string,imgURL:string,type:string){
-    this.newAnimal = {
-      id:parseInt(id),
-      title:title,
-      description:description,
-      creator:creator,
-      imgURL:imgURL,
-      type:type,
-    };
-    //emit emits a value...
-    //after some deeep thoughts i believe this is what acurally sends the data to the parent element
-    this.updateAnimalEvent.emit(this.newAnimal);
-  }
+  // addContent(title:string,description:string,creator:string,imgURL:string,type:string){
+  //   this.newAnimal = {
+  //     title:title,
+  //     description:description,
+  //     creator:creator,
+  //     imgURL:imgURL,
+  //     type:type,
+  //   };
+  //   this.newAnimalEvent.emit(this.newAnimal);
+  // }
+  // updateContent(id:string,title:string,description:string,creator:string,imgURL:string,type:string){
+  //   this.newAnimal = {
+  //     id:parseInt(id),
+  //     title:title,
+  //     description:description,
+  //     creator:creator,
+  //     imgURL:imgURL,
+  //     type:type,
+  //   };
+  //   this.updateAnimalEvent.emit(this.newAnimal);
+  // }
 }
